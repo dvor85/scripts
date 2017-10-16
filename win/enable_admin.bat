46967c4d412b83038c99e68f4651b4707bbac534
Dmitriy Vorotilin <dvor85@gmail.com>
2017-10-16 15:31:52 +0400

    Fix enable admin script

--- win/enable_admin.bat
+++ win/enable_admin.bat
@@ -3,3 +3,5 @@ net user Администратор /active && net user Администратор *
 rem Hide admins from login view
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" /v Администратор /t REG_DWORD /d 0 /f
 reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\CredUI /v EnumerateAdministrators /t REG_DWORD /d 0 /f
+reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 1 /f
+
