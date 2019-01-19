2b2564105473bfa76ff8ecb7492238a55f1f45a5
Dmitriy Vorotilin <dvor85@gmail.com>
2019-01-19 23:54:40 +0400

    a4tech_mouse for bionic

--- lin/a4tech_mouse.sh
+++ lin/a4tech_mouse.sh
@@ -2,12 +2,12 @@
 
 while true
 do
-  buttons=$(xinput --list | grep 'A4TECH USB' | grep -oE 'id=[0-9]+' | grep -oE '[0-9]+')
+  buttons=`xinput --list | sed -rn 's/.*A4TECH USB.*id=([0-9]+).*keyboard.*/\1/p'`
   for button in $buttons; do
     xinput --disable $button
-
-    event_file=$(xinput --list-props $button | grep -oE '/dev/input/event[0-9]+')
-    evtest $event_file | awk '/KEY_LEFTMETA), value 1/ {system("xdotool click --delay 100 --repeat 2 1")}'
+  
+    event_file=`xinput --list-props $button | grep -oE '/dev/input/event[0-9]+'`
+    evtest $event_file | awk '/KEY_LEFTMETA), value 1/ {system("xdotool click --delay 100 --repeat 2 1")}' &
   done;
 
   inotifywait -e create /dev/input
